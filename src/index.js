54b196993bbb87830c4f42e63ba046f7801f9346
Fred <33107804+Fredxingxing@users.noreply.github.com>
2020-09-14 19:42:36 +0800

    HMR learning

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
 import _ from 'lodash';
-import './style.css'
+import printMe from './print';
+import './styles.css';
 
 function component() {
     var element = document.createElement('div');
+    var btn = document.createElement('button');
 
     // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
+    btn.innerHTML = 'Click me and check the console';
+    btn.onclick = printMe;
+    element.appendChild(btn);
+
     return element;
 }
 
-document.body.appendChild(component());
+let element = component();
+document.body.appendChild(element);
+
+if (module.hot) {
+    module.hot.accept('./print.js', function () {
+        console.log('Acception the updated printMe module!')
+        document.body.removeChild(element);
+        element = component();
+        document.body.appendChild(element)
+    })
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,43 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, 'dist')
+    entry: {
+        app: './src/index.js',
     },
-}
+    devServer: {
+        contentBase: './dist',
+        hot: true,
+        quiet: true,
+        watchOptions: {
+            ignored: /node_modules/,
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Output Management'
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    optimization: {
+        moduleIds: 'named',
+    },
+    devtool: 'inline-source-map',
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/',
+    },
+};
